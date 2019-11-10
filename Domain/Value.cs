6732151560314f83de506f2f01215e688d425286
Deesteph55 <deedee704@ymail.com>
2019-11-10 12:16:41 -0500

    Initial Commit

---
+++ Domain/Value.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Value //Note* This is an enity because we are using entity framework
+    {
+        public int Id { get; set; }//this is a property of an entity. because its id its a primary key
+
+        public string Name { get; set; }
+    }
+}
