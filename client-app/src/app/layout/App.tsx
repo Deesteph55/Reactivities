dec8b133dd3b202515bb1f59f9d27b4dcbea4815
Deesteph55 <deedee704@ymail.com>
2019-12-26 13:44:38 -0600

    Midway Through Sec 7 with issues

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,88 +1,75 @@
-import React, { useState, useEffect, Fragment, SyntheticEvent } from "react";
+import React, {
+  useState,
+  useEffect,
+  Fragment,
+  SyntheticEvent,
+  useContext
+} from "react";
 import { Container } from "semantic-ui-react";
 import { IActivity } from "../models/activity";
 import NavBar from "../../features/nav/NavBar";
 import { ActivityDashboard } from "../../features/activities/dashboard/ActivityDashboard";
 import agent from "../api/agent";
 import { LoadingComponent } from "./LoadingComponent";
+import ActivityStore from "../stores/activityStore";
+import {observer} from 'mobx-react-lite';
 
 const App = () => {
+  const activityStore = useContext(ActivityStore);
   const [activities, setActivities] = useState<IActivity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(
-    null
-  );
-  const [editMode, setEditMode] = useState(false);
-  const [loading, setLoading] = useState(true);
+     null
+    );
+   const [editMode, setEditMode] = useState(false);
+   const [loading, setLoading] = useState(true);
   const [submitting, setSubmitting] = useState(false);
-  const [target, setTarget] = useState('');//target is going to represent the button name that is being clicked
-
-  const handleSelectActivity = (id: string) => {
-    setSelectedActivity(activities.filter(a => a.id === id)[0]);
-    setEditMode(false);
-  };
-
-  const handleOpenCreateForm = () => {
-    setSelectedActivity(null);
-    setEditMode(true);
-  };
-
-  const handleCreateActivity = (activity: IActivity) => {
-    setSubmitting(true);
-    agent.Activities.create(activity).then(() => {
-      setActivities([...activities, activity]); //this an activities array. use spread operator to take existing activities and spread the
-      //into new array and add new activity onto array
-      setSelectedActivity(activity); //after we have created the activity we are displaying its details in the activty details component
-      setEditMode(false);
-    }).then(() => setSubmitting(false))
-  };
+  const [target, setTarget] = useState(""); //target is going to represent the button name that is being clicked
 
   const handleEditActivity = (activity: IActivity) => {
     setSubmitting(true);
-    agent.Activities.update(activity).then(() => {
-      setActivities([...activities.filter(a => a.id !== activity.id), activity]);
-    //update specific activity
-    //first part of array is: spread activities and filter out activity we are updating
-    //so array is gonna contain activities with ids that do not match specified id of activity we are passing in
-    //then we tack on newly updated activity
-    setSelectedActivity(activity); //after we have created the activity we are displaying its details in the activty details component
-    setEditMode(false);
-    }).then(() => setSubmitting(false))
-  }
+    agent.Activities.update(activity)
+      .then(() => {
+        setActivities([
+          ...activities.filter(a => a.id !== activity.id),
+          activity
+        ]);
+        //update specific activity
+        //first part of array is: spread activities and filter out activity we are updating
+        //so array is gonna contain activities with ids that do not match specified id of activity we are passing in
+        //then we tack on newly updated activity
+        setSelectedActivity(activity); //after we have created the activity we are displaying its details in the activty details component
+        setEditMode(false);
+      })
+      .then(() => setSubmitting(false));
+  };
 
-  const handleDeleteActivity = (event: SyntheticEvent<HTMLButtonElement>, id: string) => {
+  const handleDeleteActivity = (
+    event: SyntheticEvent<HTMLButtonElement>,
+    id: string
+  ) => {
     setSubmitting(true);
-    setTarget(event.currentTarget.name)//when you click button, set target to the name of button
-    agent.Activities.delete(id).then(() => {
-      setActivities([...activities.filter(a => a.id !== id)]); //want to return other activities that do not match
-    }).then(() => setSubmitting(false))
+    setTarget(event.currentTarget.name); //when you click button, set target to the name of button
+    agent.Activities.delete(id)
+      .then(() => {
+        setActivities([...activities.filter(a => a.id !== id)]); //want to return other activities that do not match
+      })
+      .then(() => setSubmitting(false));
   };
 
   useEffect(() => {
     //gets our activities
-    agent.Activities.list().then(response => {
-      let activities: IActivity[] = [];
-      response.forEach((activity) => {
-        activity.date = activity.date.split(".")[0];
-        activities.push(activity);
-      });
-      setActivities(activities);
-    }).then(() => setLoading(false));
-  }, []);
+    activityStore.loadActivities();
+  }, [activityStore]);
 
-  if (loading) return <LoadingComponent content='Loading activities...'/> 
+  if (activityStore.loadingInitial) return <LoadingComponent content="Loading activities..." />;
 
   return (
     <Fragment>
-      <NavBar openCreateForm={handleOpenCreateForm} />
+      <NavBar/>
       <Container style={{ marginTop: "7em" }}>
         <ActivityDashboard
-          activities={activities}
-          selectActivity={handleSelectActivity}
-          selectedActivity={selectedActivity}
-          editMode={editMode}
           setEditMode={setEditMode}
           setSelectedActivity={setSelectedActivity}
-          createActivity={handleCreateActivity}
           editActivity={handleEditActivity}
           deleteActivity={handleDeleteActivity}
           submitting={submitting}
@@ -93,4 +80,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default observer(App);
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,18 +1,17 @@
-import React, { SyntheticEvent } from "react";
+import React, { SyntheticEvent, useContext } from "react";
 import { Grid } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/activity";
 import { ActivityList } from "./ActivityList";
 import { ActivityDetails } from "../details/ActivityDetails";
 import ActivityForm from "../form/ActivityForm";
+import {observer} from 'mobx-react-lite';
+import ActivityStore from '../../../app/stores/activityStore';
 
 interface IProps {
-  activities: IActivity[];
-  selectActivity: (id: string) => void; //selectac is a function that returns void. we have tell iprops what kind of thing we are expecting from selectac
-  selectedActivity: IActivity | null;
-  editMode: boolean;
+  // activities: IActivity[];
+  // selectActivity: (id: string) => void; //selectac is a function that returns void. we have tell iprops what kind of thing we are expecting from selectac
   setEditMode: (editMode: boolean) => void;
   setSelectedActivity: (activity: IActivity | null) => void;
-  createActivity: (activity: IActivity) => void;
   editActivity: (activity: IActivity) => void;
   deleteActivity: (e:SyntheticEvent<HTMLButtonElement>, id: string) => void;
   submitting: boolean;
@@ -20,24 +19,19 @@ interface IProps {
 }
 
 export const ActivityDashboard: React.FC<IProps> = ({
-  activities,
-  selectActivity,
-  selectedActivity,
-  editMode,
   setEditMode,
   setSelectedActivity,
-  createActivity,
   editActivity,
   deleteActivity,
   submitting,
   target
 }) => {
+  const activityStore = useContext(ActivityStore);
+  const {editMode, selectedActivity} = activityStore;
   return (
     <Grid>
       <Grid.Column width={10}>
         <ActivityList
-          activities={activities}
-          selectActivity={selectActivity}
           deleteActivity={deleteActivity}
           submitting={submitting}
           target={target}
@@ -46,7 +40,6 @@ export const ActivityDashboard: React.FC<IProps> = ({
       <Grid.Column width={6}>
         {selectedActivity && !editMode && (
           <ActivityDetails
-            activity={selectedActivity}
             setEditMode={setEditMode}
             setSelectedActivity={setSelectedActivity}
           />
@@ -57,7 +50,6 @@ export const ActivityDashboard: React.FC<IProps> = ({
             key={(selectedActivity && selectedActivity.id) || 0}
             setEditMode={setEditMode}
             activity={selectedActivity!}
-            createActivity={createActivity}
             editActivity={editActivity}
             submitting={submitting}
           />
@@ -68,4 +60,4 @@ export const ActivityDashboard: React.FC<IProps> = ({
   );
 };
 
-export default ActivityDashboard;
+export default observer(ActivityDashboard);
